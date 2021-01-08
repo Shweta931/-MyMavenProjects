d22ce799000a9c7c87b4da43ee5ec055fe67bc85
Raj <Raj@DESKTOP-ULL6IFD>
2021-01-08 18:43:58 +0530

    ForgotPassword file added

---
+++ src/test/java/com/scripts/facebook/ForgotPassword.java
@@ -0,0 +1,14 @@
+package com.scripts.facebook;
+
+import org.testng.annotations.Test;
+
+public class ForgotPassword 
+{
+	@Test
+	public void forgotpassword() 
+	{
+		System.out.println("I forgot my password");
+		
+	}
+
+}
